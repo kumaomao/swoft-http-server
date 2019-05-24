cdac5a0bb33e9e8314f360268a99f98cb399a2c2
kumaomao <z1083271136@163.com>
2019-05-24 17:46:57 +0800

    获取GET数据

--- src/Middleware/ValidatorMiddleware.php
+++ src/Middleware/ValidatorMiddleware.php
@@ -48,7 +48,20 @@ class ValidatorMiddleware implements MiddlewareInterface
         $handlerId = $route->getHandler();
         [$className, $method] = explode('@', $handlerId);
 
-        $data = $request->getParsedBody();
+        //获取路由上定义的参数
+        $attributes = $request->getAttributes();
+        $attributes = $attributes["swoftRouterHandler"][2]->getParams();
+        //这里只获取了POST提交参数的数据
+        //原想法是判断提交方式来获取不同的数据，但是提交方式不止POST,GET
+        $body = $request->getParsedBody();
+
+        //这里先获取body，body无值再获取QueryParams上的值
+        $params = [];
+        if(!$body){
+            $params = $request->getQueryParams();
+        }
+        //合并数组
+        $data = array_merge($attributes,$body,$params);
 
         // Fix body is empty string
         $data = empty($data) ? [] : $data;
